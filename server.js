08f794eb9862339de754f989723ee5f113fa643a
Baraiya Sameep <baraiya.sameep@gmail.com>
2020-12-30 20:27:53 +0530

    heroku fix

--- server.js
+++ server.js
@@ -68,6 +68,11 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(__dirname + '/client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/client/build/index.html');
+});
+
 app.listen(
   PORT,
   console.log(
